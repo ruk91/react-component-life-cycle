6b7169e2353f5549cfb4be9fc192421fed682323
ruk91 <sajithkasn@gmail.com>
2022-09-15 21:37:11 +0530

    Frtch data in react js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Text from './Text';
+import Axios from 'axios'
 
 function App() {
   const [showText, setShowText] = useState(false)
+  const [catFact, setCatFact] = useState('')
+
+  // useEffect(() => {
+  //   fetch('https://catfact.ninja/fact')
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log('data: ', data)
+  //   })
+  // }, [])
+
+  // useEffect(() => {
+  //   Axios('https://catfact.ninja/fact')
+  //   .then((res) => {
+  //     console.log("res.data: ", res.data)
+  //     setCatFact(res.data.fact)
+  //   })
+  // }, [])
+
+  useEffect(() => {
+    fetchCatFact()
+  }, [])
+  
+  const fetchCatFact = () => {
+    Axios('https://catfact.ninja/fact')
+    .then((res) => {
+      console.log("res.data: ", res.data)
+      setCatFact(res.data.fact)
+    })
+  }
 
   return (
     <div className="App">
       <button
-        onClick={() => {
-          setShowText(!showText)
-        }}
+        // onClick={() => {
+        //   setShowText(!showText)
+        // }}
+        onClick={fetchCatFact}
       >
-        Show Text
+        Generate Cat Fact
       </button>
+      <h3>{catFact}</h3>
 
       {
         showText && <Text />
