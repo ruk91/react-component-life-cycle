28f0278f9346e4f4f5e6bd8af5001dd2dddd74c8
ruk91 <sajithkasn@gmail.com>
2022-09-21 11:57:14 +0530

    ➕ Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import { useState } from 'react'
-import Text from './Text';
 
 function App() {
-  const [showText, setShowText] = useState(false)
 
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setShowText(!showText)
-        }}
-      >
-        Show Text
-      </button>
-
-      {
-        showText && <Text />
-      }
+    
     </div>
   );
 }
